5bfa0fe695f1b51406cd723257cd206e55c7c53e
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-11-23 19:04:10 +0300

    0.4 - Connect DataBase

--- GameParadise/GameParadise/settings.py
+++ GameParadise/GameParadise/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'GameParadise.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gameparadise',
+        'USER': 'postgres',
+        'PASSWORD': '1r2u3s4s5i6a',
+        'HOST': 'localhost',
+        'PORT': '45670',
     }
 }
 
--- GameParadise/catalog/views.py
+++ GameParadise/catalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from catalog.models import Game
 
 
 def index(request):
+    games = Game.objects.raw('Select "id", "name", "price", "sold" FROM "catalog_game"')
     template = 'catalog/index.html'
-    return render(request, template)
+    return render(request, template, {'games': games})
