ed741fa47979a3786ecc80fabbae5447f0355c1b
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-11-23 20:05:25 +0300

    0.42 - Update Site View

--- GameParadise/inventory/views.py
+++ GameParadise/inventory/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from catalog.models import Game
 
 
 def inventory(request):
+    games = Game.objects.raw('Select "id", "name", "price", "sold" FROM "catalog_game"')
     template = 'inventory/inventory.html'
-    return render(request, template)
+    return render(request, template, {'games': games})
