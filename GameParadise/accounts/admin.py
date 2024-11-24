b858d40d5f7484576a4a689be660dd31c2aabeca
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-11-25 00:08:23 +0300

    0.9 - Set Up Django admin panel with custom data

--- GameParadise/accounts/admin.py
+++ GameParadise/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Client
 
-# Register your models here.
+
+admin.site.register(Client)
+
+# @admin.register(Client)
+# class ClientAdmin(admin.ModelAdmin):
+#     pass
--- GameParadise/catalog/admin.py
+++ GameParadise/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+
+admin.site.register(Game)
+
+# @admin.register(Game)
+# class GameAdmin(admin.ModelAdmin):
+#     pass
