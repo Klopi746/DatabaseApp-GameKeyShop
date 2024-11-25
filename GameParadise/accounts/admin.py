0360ab8102855d69a2b55b2885c70ad6fde673e5
Steel Bocharov <132206705+Klopi746@users.noreply.github.com>
2024-11-25 11:08:00 +0300

    0.91 - Simple administrator role

--- GameParadise/accounts/admin.py
+++ GameParadise/accounts/admin.py
@@ -3,7 +3,16 @@ from .models import Client
 
 
 admin.site.register(Client)
-
 # @admin.register(Client)
 # class ClientAdmin(admin.ModelAdmin):
-#     pass
+#     list_display = ['mail']
+#     readonly_fields = ['mail']  # Делаем поля только для чтения
+
+#     def has_add_permission(self, request):
+#         return False  # Запрещаем добавлять
+
+#     def has_change_permission(self, request, obj=None):
+#         return False  # Запрещаем изменять
+
+#     def has_delete_permission(self, request, obj=None):
+#         return False  # Запрещаем удалять
